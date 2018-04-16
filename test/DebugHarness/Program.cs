6b2c8036d8af2b616723a80abd32ad98be9e3e96
Brian Robbins <brianrob@microsoft.com>
2018-04-16 15:52:03 -0700

    Add DebugHarness.

---
+++ test/DebugHarness/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using NETCore.Tracing;
+
+namespace DebugHarness
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Control.Start();
+
+            Console.WriteLine("Started Tracing.  Press any key to exit.");
+            Console.ReadKey();
+
+            Control.Stop();
+        }
+    }
+}
