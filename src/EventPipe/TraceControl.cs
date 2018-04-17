b6b27fb112dacbe0649f21c6489931cdb693ca58
Brian Robbins <brianrob@microsoft.com>
2018-04-17 13:28:07 -0700

    Add fallback GUIDs to TraceControl.EnableDefault.

--- src/EventPipe/TraceControl.cs
+++ src/EventPipe/TraceControl.cs
@@ -24,26 +24,34 @@ namespace NETCore.Tracing.EventPipe
 
             // Create a new instance of EventPipeConfiguration.
             TraceConfiguration config = new TraceConfiguration(outputFile, circularBufferMB);
+
             // Setup the provider values.
+
             // Public provider.
             string providerName="Microsoft-Windows-DotNETRuntime";
             UInt64 keywords = 0x4c14fccbd;
+            config.EnableProvider(providerName, keywords, level);
 
-            // Enable the provider.
+            // Fallback GUID for .NET Core 2.0.
+            providerName = "e13c0d23-ccbc-4e12-931b-d9cc2eee27e4";
             config.EnableProvider(providerName, keywords, level);
 
             // Private provider.
             providerName = "Microsoft-Windows-DotNETRuntimePrivate";
             keywords = 0x4002000b;
+            config.EnableProvider(providerName, keywords, level);
 
-            // Enable the provider.
+            // Fallback GUID for .NET Core 2.0.
+            providerName = "763fd754-7086-4dfe-95eb-c01a46faf4ca";
             config.EnableProvider(providerName, keywords, level);
 
             // Sample profiler.
             providerName = "Microsoft-DotNETCore-SampleProfiler";
             keywords = 0x0;
+            config.EnableProvider(providerName, keywords, level);
 
-            // Enable the provider.
+            // Fallback GUID for .NET Core 2.0.
+            providerName = "3c530d44-97ae-513a-1e6d-783e8f8e03a9";
             config.EnableProvider(providerName, keywords, level);
 
             // Set the sampling rate.
