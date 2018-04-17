d0617b4286ec807fec297b5aa06143555a20a319
Brian Robbins <brianrob@microsoft.com>
2018-04-17 13:28:56 -0700

    Add two spinner threads to DebugHarness.

--- test/DebugHarness/Program.cs
+++ test/DebugHarness/Program.cs
@@ -1,18 +1,34 @@
 ﻿using System;
+using System.Threading.Tasks;
 using NETCore.Tracing;
 
 namespace DebugHarness
 {
     class Program
     {
+        private const int NumTasks = 2;
+
         static void Main(string[] args)
         {
             Control.Start();
+
+            for(int i=0; i<NumTasks; i++)
+            {
+                Task.Run(new Action(SpinTask));
+            }
 
             Console.WriteLine("Started Tracing.  Press any key to exit.");
             Console.ReadKey();
 
             Control.Stop();
+        }
+
+        private static void SpinTask()
+        {
+            while(true)
+            {
+                GC.KeepAlive(new object());
+            }
         }
     }
 }
