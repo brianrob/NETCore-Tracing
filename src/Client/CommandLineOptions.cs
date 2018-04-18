62c7f0198d70d02616db9773698c3989f2563cea
Brian Robbins <brianrob@microsoft.com>
2018-04-18 16:49:08 -0700

    Add client application.

---
+++ src/Client/CommandLineOptions.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using CommandLine;
+
+namespace NETCore.Tracing.Client
+{
+    [Verb("EnableTracing", HelpText = "Enable EventPipe tracing functionality.")]
+    internal sealed class EnableTracingOptions
+    {
+        [Option("OutputFileName", HelpText = "The name of the destination file without the full path.")]
+        public string OutputFileName { get; set; }
+
+        [Option("Providers", HelpText = "A set of comma-delimited provider configurations.  For example: Microsoft-Windows-DotNETRuntime:0xFFFFFFFFFFFFFFFF:5,...")]
+        public string Providers { get; set; }
+
+        [Option("CircularMB", HelpText = "The size in megabytes of the circular buffer.")]
+        public uint? CircularMB { get; set; }
+
+        [Option("SamplingRate", HelpText = "The sampling rate of the sample-based profiler.")]
+        public uint? SamplingRate { get; set; }
+
+        public bool ArgumentsSpecified()
+        {
+            return !(string.IsNullOrEmpty(OutputFileName) &&
+                     string.IsNullOrEmpty(Providers) &&
+                     !CircularMB.HasValue &&
+                     !SamplingRate.HasValue);
+        }
+    }
+
+    [Verb("DisableTracing", HelpText = "Disable EventPipe tracing functionality.")]
+    internal sealed class DisableTracingOptions
+    {
+    }
+
+    [Verb("TriggerGC", HelpText = "Trigger a GC in the target process.")]
+    internal sealed class TriggerGCOptions
+    {
+        [Option("Generation", Required = false, HelpText = "The generation of the GC to trigger.")]
+        public uint? Generation { get; set; }
+    }
+}
---
+++ src/Client/Program.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using CommandLine;
+
+namespace NETCore.Tracing.Client
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            CommandLine.Parser.Default.ParseArguments<EnableTracingOptions, DisableTracingOptions, TriggerGCOptions>(args)
+                .MapResult(
+                    (EnableTracingOptions opts) => EnableTracing(opts),
+                    (DisableTracingOptions opts) => DisableTracing(opts),
+                    (TriggerGCOptions opts) => TriggerGC(opts),
+                    errs => 1);
+        }
+
+        private static int EnableTracing(EnableTracingOptions options)
+        {
+            StringBuilder relativeUrlBuilder = new StringBuilder("/TraceControl/Enable");
+            int argPos = 0;
+            if (!string.IsNullOrEmpty(options.Providers))
+            {
+                if(argPos++ == 0)
+                {
+                    relativeUrlBuilder.Append($"?Providers={options.Providers}");
+                }
+                else
+                {
+                    relativeUrlBuilder.Append($"&Providers={options.Providers}");
+                }
+            }
+
+            if (!string.IsNullOrEmpty(options.OutputFileName))
+            {
+                if (argPos++ == 0)
+                {
+                    relativeUrlBuilder.Append($"?OutputFileName={options.OutputFileName}");
+                }
+                else
+                {
+                    relativeUrlBuilder.Append($"&OutputFileName={options.OutputFileName}");
+                }
+            }
+
+            if(options.CircularMB.HasValue)
+            {
+                if (argPos++ == 0)
+                {
+                    relativeUrlBuilder.Append($"?CircularMB={options.CircularMB.Value}");
+                }
+                else
+                {
+                    relativeUrlBuilder.Append($"&CircularMB={options.CircularMB.Value}");
+                }
+            }
+
+            if (options.SamplingRate.HasValue)
+            {
+                if (argPos++ == 0)
+                {
+                    relativeUrlBuilder.Append($"?SamplingRate={options.SamplingRate.Value}");
+                }
+                else
+                {
+                    relativeUrlBuilder.Append($"&SamplingRate={options.SamplingRate.Value}");
+                }
+            }
+
+            string response = ExecuteWebRequest(relativeUrlBuilder.ToString());
+            Console.WriteLine(response);
+
+            // TODO: Get and display tracing session properties.
+            return 0;
+        }
+
+        private static int DisableTracing(DisableTracingOptions options)
+        {
+            string relativeUrl = "/TraceControl/Disable";
+            string response = ExecuteWebRequest(relativeUrl);
+            Console.WriteLine(response);
+
+            // TODO: Get and display the trace file location.
+            return 0;
+        }
+
+        private static int TriggerGC(TriggerGCOptions options)
+        {
+            // Get the generation to collect.
+            uint generation = options.Generation ?? 2;
+
+            // Build the relative URL to hit.
+            string relativeUrl = $"/TriggerGC/{generation}";
+            string response = ExecuteWebRequest(relativeUrl);
+            Console.WriteLine(response);
+
+            return 0;
+        }
+
+        private static string ExecuteWebRequest(string relativeUrl)
+        {
+            string targetUrl = BaseUrl + relativeUrl;
+            Console.WriteLine($"RequestURL = {targetUrl}");
+
+            using (HttpClient client = new HttpClient())
+            {
+                string response = null;
+                try
+                {
+                    Task<HttpResponseMessage> responseMessageTask = client.GetAsync(targetUrl);
+                    responseMessageTask.Wait();
+                    HttpResponseMessage responseMessage = responseMessageTask.Result;
+                    Task<string> responseTask = responseMessage.Content.ReadAsStringAsync();
+                    responseTask.Wait();
+                    response = responseTask.Result;
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.ToString());
+                }
+
+                return response;
+            }
+        }
+
+        private const string BaseUrl = "http://localhost:8080";
+    }
+}
